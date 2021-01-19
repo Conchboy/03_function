c3ffc183bd1eab526d496ae20b58b2f466ddced4
conchboy <panchaocn@hotmail.com>
2021-01-19 18:29:21 +0800

    Create README.md

--- rp_14_遍历字典.py
+++ rp_14_遍历字典.py
@@ -6,7 +6,7 @@ xiaoming_dict = {"name": "小明",
 # 变量k是每一次循环中，获得到的键值对的key
 
 
-for k in xiaoming_dict:
+for k in xiaoming_dict: # 这里的变量k是获得的键值
     print("%-6s ： %s" % (k, xiaoming_dict[k]))
 # 实际开发常用的应用场景：
 # 使用字典存储描述一个物体（对象）的相关信息：比如一个学生的相关信息
