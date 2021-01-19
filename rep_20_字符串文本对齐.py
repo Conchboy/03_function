f4bd11846fec420d97685be6a66e7e4c4a76efee
conchboy <panchaocn@hotmail.com>
2021-01-20 00:27:21 +0800

    Create README.md

--- rep_20_字符串文本对齐.py
+++ rep_20_字符串文本对齐.py
@@ -2,15 +2,15 @@
 # 要求，顺序并居中对齐输出以下内容
 poem = ["\t\n登鹳雀楼",
         "王之涣",
-        "白日依山尽",
-        "黄河\t入海流",
-        "欲穷千\n里目",
+        "\n白日依山尽",
+        "黄河入海流",
+        "欲穷千里目\t",
         "更上一层楼"]
 
 for poem_str in poem:
     # 先使用strip方法去除字符串中的空白字符
     # 再使用center方法居中
-    print("|%s|" % poem_str.strip().center(10, "　"))
+    print("|%s|" % poem_str.strip().center(15, "　"))
 
 
 """
